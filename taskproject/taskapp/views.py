38f9465c536965c7c6a41594520f161f2c251ee7
ksi2564 <kimsungin5@gmail.com>
2022-01-12 01:46:44 +0900

    learning TemplateView

--- taskproject/taskapp/views.py
+++ taskproject/taskapp/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from .models import Task, ChecklistItem
 
 # Create your views here.
+
+
 def index(request):
     context = {}
     return render(request, 'pages/index.html', context)
+
+
+class TaskListView(TemplateView):
+    template_name = 'pages/task_list.html'
+
+    def get_context_data(self, **kwargs):
+        tasks = Task.objects.all()
+        return {
+            'tasks': tasks
+        }
