66977a844fe6bc54a424f0063aff5f8f83726f9a
ksi2564 <kimsungin5@gmail.com>
2022-02-09 21:58:55 +0900

    learning Pagination, TemplateFilter

--- taskproject/taskapp/views.py
+++ taskproject/taskapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, ListView
 from .models import Task, ChecklistItem
 from django.utils import timezone
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -17,8 +18,12 @@ class TaskListView(TemplateView):
 
     def get_context_data(self, **kwargs):
         tasks = Task.objects.filter(due__gte=timezone.now()).order_by('due').all()
+        paginator = Paginator(tasks, 4)
+        page_number = self.request.GET.get('page', '1')
+        paging = paginator.get_page(page_number)
+
         return {
-            'tasks': tasks
+            'paging': paging
         }
 
 
@@ -33,3 +38,4 @@ class TaskPreviousListView(ListView):
     model = Task
     template_name = 'pages/task_previous_list.html'
     queryset = Task.objects.filter(due__lt=timezone.now()).order_by('due')
+    paginate_by = 4
